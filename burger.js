7f65c46558c842de8260942c6462a214bec899b5
Muhammadyusuf <ibnhasaniy@gmail.com>
2022-02-23 09:48:58 +0500

    All sections are responsive for mobile devices

---
+++ burger.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+
+    });
+}
+
+navSlide();
